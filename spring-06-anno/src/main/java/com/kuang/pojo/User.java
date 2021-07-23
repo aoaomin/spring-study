aa843b08f7b523e4ade5f7264abf2f15583f307e
aoaomin <496750404@qq.com>
2021-07-23 15:42:54 +0800

    15:42

---
+++ spring-06-anno/src/main/java/com/kuang/pojo/User.java
@@ -0,0 +1,11 @@
+package com.kuang.pojo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+    @Value("章傲")
+    public String name;
+
+}
